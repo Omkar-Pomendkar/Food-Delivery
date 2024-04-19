7c33f1f277e1df0bbb7cdd0862950124a1115b49
Omkar Pomendkar <omkarpomendkar100@gmail.com>
2024-04-19 19:05:02 +0530

    updated code making use of Context API

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 const Home = () => {
+
+    // Here we are passing category and State category as a prop
+    const [category,setCategory] = useState("All");
   return (
     <div>
       <Header/>
-      <ExploreMenu/>
+      <ExploreMenu category={category} setCategory = {setCategory} />
     </div>
   )
 }
